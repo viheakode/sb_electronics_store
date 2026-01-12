2523e090585c6264d4305297a6fd13f1dc38fb6e
viheakode <viheakode787@gmail.com>
2026-01-12 23:02:01 +0700

    add auth config

--- src/main/java/com/viheakode/electronic_store/config/SecurityConfig.java
+++ src/main/java/com/viheakode/electronic_store/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/v1/product/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
